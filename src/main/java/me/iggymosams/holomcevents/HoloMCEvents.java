cbc471fea93d53e4ec7bc63cb2fedc28cc16a69d
iggymosams <sammipatnick@gmail.com>
2022-05-29 01:11:11 +0100

    feat: Created EventManager and BlockParty Event

--- src/main/java/me/iggymosams/holomcevents/HoloMCEvents.java
+++ src/main/java/me/iggymosams/holomcevents/HoloMCEvents.java
@@ -1,17 +1,45 @@
 package me.iggymosams.holomcevents;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HoloMCEvents extends JavaPlugin {
 
+    private static HoloMCEvents plugin;
+
+    EventManager eventManager = new EventManager();
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plugin = this;
+        getServer().getMessenger().registerOutgoingPluginChannel( this, "BungeeCord" ); 
+        getServer().getMessenger().registerIncomingPluginChannel( this, "my:events", new PluginMessage() ); 
+        
+        RegisterCommands();
+        RegisterEvents();
+    }
 
+    private void RegisterCommands() {
+        getCommand("start").setExecutor(eventManager);
+    }
+
+    private void RegisterEvents() {
+        getLogger().info("Registering Events");
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(eventManager, this);
+        pm.registerEvents(eventManager.blockParty, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static HoloMCEvents getPluginMain(){
+        return plugin;
+    }
+
+    public EventManager getEventManager() {
+        return eventManager;
     }
 }

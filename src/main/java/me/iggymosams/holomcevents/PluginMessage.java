ff2fb9c57c06289ef7c5c46f7fed78298eb278de
iggymosams <sammipatnick@gmail.com>
2022-06-02 15:27:03 +0100

    refactor: Auto Formated Code

--- src/main/java/me/iggymosams/holomcevents/PluginMessage.java
+++ src/main/java/me/iggymosams/holomcevents/PluginMessage.java
@@ -15,52 +15,51 @@ public class PluginMessage implements PluginMessageListener {
     @Override
     public void onPluginMessageReceived(String channel, Player player, byte[] bytes) {
         System.out.println("Message Received");
-        if (!channel.equalsIgnoreCase("my:events")) return;
+        if (!channel.equalsIgnoreCase("my:events"))
+            return;
 
         ByteArrayDataInput in = ByteStreams.newDataInput(bytes);
         String subChannel = in.readUTF();
-        if ( subChannel.equalsIgnoreCase("hosting" )) {
+        if (subChannel.equalsIgnoreCase("hosting")) {
             String host = in.readUTF();
             int msgid = in.readInt();
-//            eventManager.host = Bukkit.getPlayer(host);
+            // eventManager.host = Bukkit.getPlayer(host);
             System.out.println("T");
-            eventManager.EventSetup(Bukkit.getPlayer(host));
-        } else if ( subChannel.equalsIgnoreCase("iseventrunning")) {
+            // eventManager.EventSetup(Bukkit.getPlayer(host));
+        } else if (subChannel.equalsIgnoreCase("iseventrunning")) {
             String request = in.readUTF();
             int msgid = in.readInt();
-//            returnEventData(player, "returnEventData", EventManager.getEventType());
-        }
-        else if ( subChannel.equalsIgnoreCase("joinEvent")) {
+            // returnEventData(player, "returnEventData", EventManager.getEventType());
+        } else if (subChannel.equalsIgnoreCase("joinEvent")) {
             String request = in.readUTF();
             int msgid = in.readInt();
             eventManager.joinEvent(Bukkit.getPlayer(request));
-        }
-        else {
+        } else {
             String request = in.readUTF();
             int msgid = in.readInt();
         }
     }
 
-    public static void connect(Player player, String server){
+    public static void connect(Player player, String server) {
         ByteArrayDataOutput output = ByteStreams.newDataOutput();
         output.writeUTF("Connect");
         output.writeUTF(server);
         player.sendPluginMessage(api.getPlugin(), "BungeeCord", output.toByteArray());
     }
 
-    public static void sendEventBroadcast( Player player, String channel, String message ){
+    public static void sendEventBroadcast(Player player, String channel, String message) {
         ByteArrayDataOutput out = ByteStreams.newDataOutput();
         out.writeUTF(channel);
-        out.writeUTF( message );
-        out.writeUTF( player.getName() );
+        out.writeUTF(message);
+        out.writeUTF(player.getName());
         player.sendPluginMessage(api.getPlugin(), "BungeeCord", out.toByteArray());
     }
 
-    public static void returnEventData( Player player, String channel, String data ){
+    public static void returnEventData(Player player, String channel, String data) {
         ByteArrayDataOutput out = ByteStreams.newDataOutput();
         out.writeUTF(channel);
-        out.writeUTF( data );
-        //out.writeUTF( player.getName() );
-        player.sendPluginMessage(api.getPlugin(), "BungeeCord", out.toByteArray() );
+        out.writeUTF(data);
+        // out.writeUTF( player.getName() );
+        player.sendPluginMessage(api.getPlugin(), "BungeeCord", out.toByteArray());
     }
 }
